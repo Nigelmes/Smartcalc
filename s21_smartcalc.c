3d92a32cbe2c2db7e110639c0246ed7fe3f506d8
leftrana <leftrana@student.21-school.ru>
2022-10-20 12:54:12 +0300

    develop-s21_smartcalc.c

--- s21_smartcalc.c
+++ s21_smartcalc.c
@@ -88,31 +88,23 @@ int buffering_number(const char* src_string, char* out) {
 
 int position_counter(char src_string) {
   char* operators = "+-/*%^@ABCDEFGH()";
-  int counter = 0;
+  int counter = 0, triger = 0;
   while(operators[counter]) {
     if(operators[counter] == src_string) {
-      counter++;
+      triger = 1;
       break;
     }
     counter++;
   }
-
+  return triger?counter + 1 : 0;
 }
-
-/*  +,-
-*,/
-^
-cos,sin,tg,ctg,ln,log,!
-()
-
-То, что ниже - более высокий приоритет, по горизонтали - одинаковый.
-Корень - это частный случай степени. */
-
 
 int priority_check(char src_string) {
  int prior = 0;
-  int position_num = position_counter(src_string);
-  if (position_num > 15)
+int position_num = position_counter(src_string);
+  if (position_num == 0)
+    prior = 0;
+  else if (position_num > 15)
     prior = 5;
   else if(position_num > 6)
     prior = 4;
@@ -124,6 +116,15 @@ int priority_check(char src_string) {
     prior = 1;
   return prior;
 }
+
+/*  +,-
+*,/
+^
+cos,sin,tg,ctg,ln,log,!
+()
+
+То, что ниже - более высокий приоритет, по горизонтали - одинаковый.
+Корень - это частный случай степени. */
 
 stack_type * parser(const char* calculation_src) {
   int i = 0;
@@ -174,3 +175,5 @@ int main(void) {
     printf("Ошибка");
   return 0;
 }
+
+//  "-255.55+39.45*5555/158+A(55.66+15.78)"
