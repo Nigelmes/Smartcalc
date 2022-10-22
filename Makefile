bfd29f45113ad050b7bea8e94fedc5aac43ac6d5
Left Ranae <leftrana@ev-j5.kzn.21-school.ru>
2022-10-22 18:24:12 +0300

    develop-s21_smartcalc.c

--- Makefile
+++ Makefile
@@ -5,15 +5,22 @@ all:
 	gcc s21_smartcalc.c -o calc
 	./calc
 
-g:
+g: clean
 	git add .
 	git commit -m"develop-s21_smartcalc.c"
 	git push origin leftrana
 
 t:
 	$(CC) $(CFLAGS) s21_smartcalc.c -o calc
-#	CK_FORK=no valgrind --vgdb=no --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose ./calc
 	./calc
+
 v:
 	$(CC) $(CFLAGS) s21_smartcalc.c -o calc
 	CK_FORK=no valgrind --vgdb=no --leak-check=full --show-leak-kinds=all --track-origins=yes --verbose ./calc
+
+c:
+	clang-format -i *.c
+	make t
+
+clean:
+	rm -rf calc.dSYM calc
--- s21_smartcalc.c
+++ s21_smartcalc.c
@@ -9,10 +9,10 @@
 typedef struct Node_stack {
   double value;
   int priority;
-  struct Node_stack* next;
+  struct Node_stack *next;
 } stack_type;
 
-int validator(const char* str) {
+int validator(const char *str) {
   int errcode = 0;
   int operand = 0, i = 0;
   while (str[i]) {
@@ -26,7 +26,6 @@ int validator(const char* str) {
   if (operand != 0) errcode = 1;
   return errcode;
 }
-
 
 /*  Резерв
 void push(N** head, double value, int prior) {
@@ -58,10 +57,10 @@ N* peek(N** head) {
 }
 */
 
-stack_type *push_my(stack_type *plist , double value, int priority) {
+stack_type *push_my(stack_type *plist, double value, int priority) {
   stack_type *Part = malloc(sizeof(stack_type));
   if (Part == NULL) {
-    free (Part);
+    free(Part);
     exit(1);
   }
   Part->next = plist;
@@ -70,9 +69,10 @@ stack_type *push_my(stack_type *plist , double value, int priority) {
   return Part;
 }
 
-int buffering_number(const char* src_string, char* out) {
+int buffering_number(const char *src_string, char *out) {
   int i = 0;
-  while ((src_string[i] >= '0' && src_string[i] <= '9') || src_string[i] == '.') {
+  while ((src_string[i] >= '0' && src_string[i] <= '9') ||
+         src_string[i] == '.') {
     out[i] = src_string[i];
     i++;
   }
@@ -80,32 +80,32 @@ int buffering_number(const char* src_string, char* out) {
 }
 
 int position_counter(char src_string) {
-  char* operators = OPERATIONS;
+  char *operators = OPERATIONS;
   int counter = 0, triger = 0;
-  while(operators[counter]) {
-    if(operators[counter] == src_string) {
+  while (operators[counter]) {
+    if (operators[counter] == src_string) {
       triger = 1;
       break;
     }
     counter++;
   }
-  return triger?counter + 1 : 0;
+  return triger ? counter + 1 : 0;
 }
 
 int priority_check(char src_string) {
- int prior = 0;
-int position_num = position_counter(src_string);
+  int prior = 0;
+  int position_num = position_counter(src_string);
   if (position_num == 0)
     prior = 0;
   else if (position_num > 15)
     prior = 5;
-  else if(position_num > 6)
+  else if (position_num > 6)
     prior = 4;
-  else if(position_num > 5)
+  else if (position_num > 5)
     prior = 3;
-  else if(position_num > 2)
+  else if (position_num > 2)
     prior = 2;
-  else if(position_num >= 1)
+  else if (position_num >= 1)
     prior = 1;
   return prior;
 }
@@ -139,28 +139,27 @@ stack_type * parser(const char* calculation_src) {
 }
 */
 
-stack_type parser_uno(const char* calculation_src, int* position) {
+stack_type parser_uno(const char *calculation_src, int *position) {
   stack_type stack1 = {0};
-    int priority = priority_check(calculation_src[*position]);
-    if (priority) {
-      stack1.priority = priority;
-      stack1.value = calculation_src[*position];
-    } else {
-      char buf[256] = {0};
-      *position = *position + buffering_number(&calculation_src[*position], buf);
-      double tess = atof(buf);
-      stack1.priority = priority;
-      stack1.value = tess;
-    }
+  int priority = priority_check(calculation_src[*position]);
+  if (priority) {
+    stack1.priority = priority;
+    stack1.value = calculation_src[*position];
+  } else {
+    char buf[256] = {0};
+    *position = *position + buffering_number(&calculation_src[*position], buf);
+    double tess = atof(buf);
+    stack1.priority = priority;
+    stack1.value = tess;
+  }
   return stack1;
 }
 
-
-void print_from_node(stack_type* stack1) {
-  stack_type * Ptrack = stack1;
+void print_from_node(stack_type *stack1) {
+  stack_type *Ptrack = stack1;
   printf("\n");
-  while(Ptrack){
-    if(Ptrack->priority) {
+  while (Ptrack) {
+    if (Ptrack->priority) {
       printf(" %dpri%c", Ptrack->priority, (char)Ptrack->value);
     } else {
       printf(" %dpri%.2lf", Ptrack->priority, Ptrack->value);
@@ -170,34 +169,32 @@ void print_from_node(stack_type* stack1) {
   printf("\n");
 }
 
-void destroy_node(stack_type* stack1) {
-  stack_type * Ptrack = stack1;
-  while(Ptrack){
-    stack_type * Ptrack_bac = Ptrack->next;
+void destroy_node(stack_type *stack1) {
+  stack_type *Ptrack = stack1;
+  while (Ptrack) {
+    stack_type *Ptrack_bac = Ptrack->next;
     free(Ptrack);
     Ptrack = Ptrack_bac;
   }
   free(Ptrack);
 }
 
-int calc(const char* calculation_src) {
+int calc(const char *calculation_src) {
   int position = 0;
-  stack_type* stack_operations = NULL;
-  stack_type* stack_numbers = NULL;
+  stack_type *stack_operations = NULL;
+  stack_type *stack_numbers = NULL;
   printf("\n");
-  while(calculation_src[position]) {  //  Главный цикл вычисления
+  while (calculation_src[position]) {  //  Главный цикл вычисления
     stack_type stack_buf = parser_uno(calculation_src, &position);
     if (stack_buf.priority) {  //  Если получили операцию или скобку
-      
 
       printf(" pri%doper%c", stack_buf.priority, (int)stack_buf.value);
       position++;
     } else {  //  Если получили число
-      stack_numbers = push_my(stack_numbers, stack_buf.value, stack_buf.priority);      
+      stack_numbers =
+          push_my(stack_numbers, stack_buf.value, stack_buf.priority);
       printf(" %dpri%0.2lf", stack_buf.priority, stack_buf.value);
     }
-
-
   }
   printf("\n");
 
@@ -208,7 +205,7 @@ int calc(const char* calculation_src) {
 }
 
 int main(void) {
-  const char* arr = "-255.55+39.45*5555/158+A(55.66+15.78)";
+  const char *arr = "-255.55+39.45*5555/158+A(55.66+15.78)";
   printf("%s", arr);
   if (validator(arr) == 0)
     calc(arr);
