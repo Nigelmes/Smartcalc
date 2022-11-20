ee2649ef7172061ed6135b56cd127008161acb00
Nigel Messier <nigelmes@ev-i3.kzn.21-school.ru>
2022-11-20 18:18:38 +0300

    test

---
+++ cppfront/mainwindow.h
@@ -0,0 +1,40 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QVector>
+
+extern "C" {
+   #include "s21_smartcalc.h"
+}
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    double xBegin, xEnd, h, X;
+//    int N;
+    QVector<double> x, y;
+
+private slots:
+    void digits_numbers();
+    void AC_button();
+    void func_button();
+    void equals_button();
+    void simp_math_button();
+    void graf_button();
+    void skobki();
+    void x_button_push();
+    void on_pushButton_clicked();
+};
+#endif // MAINWINDOW_H
---
+++ t.c
@@ -0,0 +1,9 @@
+#include "s21_smartcalc.h"
+
+int main(void) {
+  const char *arr = "0";
+  if ()
+    printf("Ошибка");
+  printf("%s \n", arr);
+  return 0;
+}
